be87d2ae5f925a174bc91787e218c7d346d72e23
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-06-14 08:08:01 -0300

    Set com memset, impressao corrigida, addponto espelhado

--- I_OGraph.h
+++ I_OGraph.h
@@ -1,11 +1,12 @@
 #include "Graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void ImprimirGR(TGRAFO *grafinho){
     int a,b;
     for(a = 0;a<grafinho->pontos;a++){
-        printf("ponto %d : %d |", a+1,grafinho->lig[a]);
+        printf("ponto %d : %d |", a,grafinho->lig[a]);
         for(b=0;b < grafinho->lig[a];b++){
             printf(" (%d, %d horas)",grafinho->Mapa[a][b].ponto, grafinho->Mapa[a][b].peso);
         }
@@ -13,19 +14,28 @@ void ImprimirGR(TGRAFO *grafinho){
     }
 }
 
-
 void AdicionarAresta(TGRAFO *Graf,int X,int Y,int Peso){
     Graf->Mapa[X][Graf->lig[X]].peso = Peso;
     Graf->Mapa[X][Graf->lig[X]].ponto = Y;
+    Graf->Mapa[X][Graf->lig[X]].ponto2 = X;
     Graf->lig[X] += 1;
+}
+
+void AdicionarArestaEsp(TGRAFO *Graf,int X,int Y,int Peso){
+    Graf->Mapa[X][Graf->lig[X]].peso = Peso;
+    Graf->Mapa[X][Graf->lig[X]].ponto = Y;
+    Graf->Mapa[X][Graf->lig[X]].ponto2 = X;
+
+    Graf->lig[X] += 1;
+    Graf->Mapa[Y][Graf->lig[Y]].peso = Peso;
+    Graf->Mapa[Y][Graf->lig[Y]].ponto = X;
+    Graf->Mapa[X][Graf->lig[X]].ponto2 = Y;
+    Graf->lig[Y] += 1;
 }
 
 TGRAFO* SetGR(int X){
     TGRAFO *Grafo = (TGRAFO*)malloc(sizeof(TGRAFO));
-    int a;
     Grafo->pontos = X;
-    for(a=0;a<X;a++){
-        Grafo->lig[a] = 0;
-    }
+    memset(Grafo->lig, 0, X);
     return Grafo;
 }
