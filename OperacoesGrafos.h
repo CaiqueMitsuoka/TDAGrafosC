51a43452a1f3924f673e84990f7564b9f5d45b5b
Vitor <vitormd97@gmail.com>
2016-06-14 10:55:14 -0300

    Adicionado operacao Kruskal, retornando a arvore geradora minima

--- OperacoesGrafos.h
+++ OperacoesGrafos.h
@@ -33,3 +33,47 @@ int dijkstra (int Sair,int Chegar, TGRAFO *graf){
     }
     return distancias[Chegar];
 }
+
+TGRAFO* kruskal (TGRAFO *grafinho , int nVertices)
+{
+   TGRAFO *ArvGerMinima = NULL;
+   T_Aresta pesos[MAX_ARESTAS];
+   int i , j , qtArestas , grupo1 , grupo2 , vertices[MAX_VERTICES];
+   for(i=qtArestas=0;i<nVertices;i++)
+   {
+     for(j=0;j < grafinho->lig[i];j++)
+     {
+          pesos[qtArestas].ponto = i;
+          pesos[qtArestas].ponto2 = grafinho->Mapa[i][j].ponto2;
+          pesos[qtArestas].peso = grafinho->Mapa[i][j].peso;
+          qtArestas++;
+     }
+   }
+   qsort(pesos,qtArestas,sizeof(T_Aresta),comp);
+   for(i=1;i<=nVertices;i++)
+      vertices[i] = i;
+   for(i=0;i<qtArestas;i++)
+   {
+       if(vertices[pesos[i].ponto - 1] != vertices[pesos[i].ponto2] - 1)
+       {
+         if (vertices[pesos[i].ponto - 1] < vertices[pesos[i].ponto2 - 1])
+         {
+           grupo1 = vertices[pesos[i].ponto - 1];
+           grupo2 = vertices[pesos[i].ponto2 - 1];
+         }
+         else
+         {
+           grupo1 = vertices[pesos[i].ponto2 - 1];
+           grupo2 = vertices[pesos[i].ponto - 1];
+         }
+        }
+        for (j = 0; j < nVertices; j++)
+  				if(vertices[j] == grupo2)
+  					vertices[j] = grupo1;
+
+
+   }
+   ArvGerMinima = SetGR(nVertices);
+   AdicionarAresta(ArvGerMinima , pesos[i].ponto , pesos[i].ponto2 , pesos[i].peso);
+   return ArvGerMinima;
+}
