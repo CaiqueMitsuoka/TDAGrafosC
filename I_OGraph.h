4c9bfa17f7304c5855aaa218faf110a7b231fa74
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-06-19 12:50:05 -0300

    adicionado exclusão e adição espelhada

--- I_OGraph.h
+++ I_OGraph.h
@@ -25,12 +25,56 @@ void AdicionarArestaEsp(TGRAFO *Graf,int X,int Y,int Peso){
     Graf->Mapa[X][Graf->lig[X]].peso = Peso;
     Graf->Mapa[X][Graf->lig[X]].ponto = Y;
     Graf->Mapa[X][Graf->lig[X]].ponto2 = X;
-
     Graf->lig[X] += 1;
     Graf->Mapa[Y][Graf->lig[Y]].peso = Peso;
     Graf->Mapa[Y][Graf->lig[Y]].ponto = X;
     Graf->Mapa[X][Graf->lig[X]].ponto2 = Y;
     Graf->lig[Y] += 1;
+}
+int RemoverAresta(TGRAFO *Graf,int X,int Y){
+    int a;
+    if(Graf->lig[X] == 0){
+        return 1;
+    }
+    for(a=0;a<Graf->lig[X] && Graf->Mapa[X][a].ponto != Y;a++);
+    if(Graf->Mapa[X][a].ponto != Y){
+        if(a != Y){
+            Graf->Mapa[X][a].peso = Graf->Mapa[X][Graf->lig[X] - 1].peso;
+            Graf->Mapa[X][a].ponto = Graf->Mapa[X][Graf->lig[X] - 1].ponto;
+            Graf->Mapa[X][a].ponto2 = Graf->Mapa[X][Graf->lig[X] - 1].ponto2;
+        }
+        Graf->lig[X] -= 1;
+        return 0;
+    }
+    return 1;
+}
+
+int RemoverArestaEsp(TGRAFO *Graf,int X,int Y){
+    int a;
+    if(Graf->lig[X] == 0){
+        return 1;
+    }
+    for(a=0;a<Graf->lig[X] && Graf->Mapa[X][a].ponto != Y;a++);
+    if(Graf->Mapa[X][a].ponto != Y){
+        if(a != Y){
+            Graf->Mapa[X][a].peso = Graf->Mapa[X][Graf->lig[X] - 1].peso;
+            Graf->Mapa[X][a].ponto = Graf->Mapa[X][Graf->lig[X] - 1].ponto;
+            Graf->Mapa[X][a].ponto2 = Graf->Mapa[X][Graf->lig[X] - 1].ponto2;
+        }
+        Graf->lig[X] -= 1;
+        // return 0;
+    }
+    for(a=0;a<Graf->lig[Y] && Graf->Mapa[Y][a].ponto != Y;a++);
+    if(Graf->Mapa[Y][a].ponto != X){
+        if(a != X){
+            Graf->Mapa[Y][a].peso = Graf->Mapa[Y][Graf->lig[Y] - 1].peso;
+            Graf->Mapa[Y][a].ponto = Graf->Mapa[Y][Graf->lig[Y] - 1].ponto;
+            Graf->Mapa[Y][a].ponto2 = Graf->Mapa[Y][Graf->lig[Y] - 1].ponto2;
+        }
+        Graf->lig[Y] -= 1;
+        // return 0;
+    }
+    return 1;
 }
 
 TGRAFO* SetGR(int X){
