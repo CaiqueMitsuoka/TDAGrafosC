d79c2e15382535cf9497e756e09ddd7a1e827e16
Vítor Manfré <vitormd97@gmail.com>
2016-06-19 10:04:39 -0300

    Kruskal Arrumado

--- OperacoesGrafos.h
+++ OperacoesGrafos.h
@@ -7,6 +7,18 @@ void potencia(int K, int *C){
     *C = temp;
 }
 
+int comp(const void *p1, const void *p2)
+{	T_Aresta *i = (T_Aresta *)p1, *j = (T_Aresta *)p2;
+
+	if(i->peso > j->peso)
+		return 1;
+	else
+		if(i->peso < j->peso)
+			return -1;
+		else
+			return 0;
+}
+
 int dijkstra (int Sair,int Chegar, TGRAFO *graf){
     int distancias[MAX_VERTICES], vertices[MAX_VERTICES], i, j, menor, prox, infinito;
     infinito = (sizeof(int) * 8) - 2;
@@ -38,42 +50,42 @@ TGRAFO* kruskal (TGRAFO *grafinho , int nVertices)
 {
    TGRAFO *ArvGerMinima = NULL;
    T_Aresta pesos[MAX_ARESTAS];
+   ArvGerMinima = SetGR(nVertices);
    int i , j , qtArestas , grupo1 , grupo2 , vertices[MAX_VERTICES];
    for(i=qtArestas=0;i<nVertices;i++)
    {
      for(j=0;j < grafinho->lig[i];j++)
      {
           pesos[qtArestas].ponto = i;
-          pesos[qtArestas].ponto2 = grafinho->Mapa[i][j].ponto2;
+          pesos[qtArestas].ponto2 = grafinho->Mapa[i][j].ponto;
           pesos[qtArestas].peso = grafinho->Mapa[i][j].peso;
           qtArestas++;
      }
    }
    qsort(pesos,qtArestas,sizeof(T_Aresta),comp);
-   for(i=1;i<=nVertices;i++)
-      vertices[i] = i;
+   for(i=0;i<nVertices;i++)
+     vertices[i] = i;
+
    for(i=0;i<qtArestas;i++)
    {
-       if(vertices[pesos[i].ponto - 1] != vertices[pesos[i].ponto2] - 1)
+       if(vertices[pesos[i].ponto] != vertices[pesos[i].ponto2])
        {
-         if (vertices[pesos[i].ponto - 1] < vertices[pesos[i].ponto2 - 1])
+         if (vertices[pesos[i].ponto] < vertices[pesos[i].ponto2])
          {
-           grupo1 = vertices[pesos[i].ponto - 1];
-           grupo2 = vertices[pesos[i].ponto2 - 1];
+           grupo1 = vertices[pesos[i].ponto];
+           grupo2 = vertices[pesos[i].ponto2];
          }
          else
          {
-           grupo1 = vertices[pesos[i].ponto2 - 1];
-           grupo2 = vertices[pesos[i].ponto - 1];
+           grupo1 = vertices[pesos[i].ponto2];
+           grupo2 = vertices[pesos[i].ponto];
          }
+         AdicionarAresta(ArvGerMinima , pesos[i].ponto , pesos[i].ponto2 , pesos[i].peso);
         }
+
         for (j = 0; j < nVertices; j++)
   				if(vertices[j] == grupo2)
   					vertices[j] = grupo1;
-
-
    }
-   ArvGerMinima = SetGR(nVertices);
-   AdicionarAresta(ArvGerMinima , pesos[i].ponto , pesos[i].ponto2 , pesos[i].peso);
    return ArvGerMinima;
 }
